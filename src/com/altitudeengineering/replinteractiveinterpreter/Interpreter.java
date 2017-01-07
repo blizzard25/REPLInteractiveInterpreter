7bcdf6dc48fb17f52fd56218a4c005914915de9a
Blizzard25 <evansanders@Evans-MacBook-Pro.local>
2017-01-07 01:06:32 -0700

    updatedfunctions

--- src/com/altitudeengineering/replinteractiveinterpreter/Interpreter.java
+++ src/com/altitudeengineering/replinteractiveinterpreter/Interpreter.java
@@ -8,6 +8,7 @@ import java.util.Set;
 public class Interpreter {	
 	protected final ExpressionEvaluator evaluator;
 	Scanner scanner;
+	boolean shouldBreak = false;
 	public Interpreter() {
 		this.evaluator = new ExpressionEvaluator(new HashMap<>(), new HashMap<>()); 
 	}
@@ -29,54 +30,57 @@ public class Interpreter {
 	public void input(String function, String expression, ArrayList<String> innerVars) {
 		this.evaluator.evaluate(function, expression, innerVars);
 	}
-	public static void main(String[] args) {
-		Interpreter i = new Interpreter();
-		i.scanner = new Scanner(System.in).useDelimiter("\\n");
-		System.out.println("Enter your code below.");
+	public void checkExpression(String codeLine) {
 		String printVarsCmd = "Console.printAllVars";
 		String printFuncsCmd = "Console.printAllFunctions";
 		String printCmd = "Console.printLast";
 		String lastLine = "";
+		if(codeLine.charAt(0) == 'f' && codeLine.charAt(1) == 'n' && codeLine.charAt(2) == ' ') {
+			StringBuilder sb = new StringBuilder();
+			int opIndex = codeLine.indexOf('=');
+			ArrayList<String> lhs = new ArrayList<String>();
+			for(int c = 3; c < opIndex; c++) {
+				StringBuilder lhsVals = new StringBuilder();
+				while(codeLine.charAt(c) != ' ') {
+					lhsVals.append(codeLine.charAt(c));
+					c++;
+				}
+				lhs.add(lhsVals.toString());
+			}
+			opIndex += 2;
+			for(int c = opIndex; c < codeLine.length(); c++) {
+				sb.append(codeLine.charAt(c));
+			}
+			input(lhs.get(0), sb.toString(), lhs);
+		} else if(codeLine.equals(printVarsCmd)) {
+			Set<String> vars = this.evaluator.variables.keySet();
+			for(String v : vars) {
+				System.out.print(String.format("%s = %s", v, input(v)) + " ");
+			}
+			System.out.println();
+		} else if(codeLine.equals(printFuncsCmd)) {
+			Set<ArrayList<String>> funcs = this.evaluator.functions.keySet();
+			for(ArrayList<String> fns : funcs) {
+				for(String f : fns) {
+					System.out.print(f + " ");
+				}
+				System.out.println("=> " + this.evaluator.functions.get(fns));
+			}
+		} else if(codeLine.equals(printCmd)) {
+			System.out.println(input(lastLine));
+		} else if(codeLine.equals("END")) {
+			shouldBreak = true;
+		} else input(codeLine);		
+		lastLine = codeLine;
+	}
+	public static void main(String[] args) {
+		Interpreter i = new Interpreter();
+		i.scanner = new Scanner(System.in).useDelimiter("\\n");
+		System.out.println("Enter your code below.");
 		while(true) {
 			String codeLine = i.scanner.next();
-			if(codeLine.charAt(0) == 'f' && codeLine.charAt(1) == 'n' && codeLine.charAt(2) == ' ') {
-				StringBuilder sb = new StringBuilder();
-				int opIndex = codeLine.indexOf('=');
-				ArrayList<String> lhs = new ArrayList<String>();
-				for(int c = 3; c < opIndex; c++) {
-					StringBuilder lhsVals = new StringBuilder();
-					while(codeLine.charAt(c) != ' ') {
-						lhsVals.append(codeLine.charAt(c));
-						c++;
-					}
-					lhs.add(lhsVals.toString());
-				}
-				opIndex += 2;
-				for(int c = opIndex; c < codeLine.length(); c++) {
-					sb.append(codeLine.charAt(c));
-				}
-				i.input(lhs.get(0), sb.toString(), lhs);
-			} else if(codeLine.equals("END")) {
-				break;
-			} else if(codeLine.equals(printVarsCmd)) {
-				Set<String> vars = i.evaluator.variables.keySet();
-				for(String v : vars) {
-					System.out.print(String.format("%s = %s", v, i.input(v)) + " ");
-				}
-				System.out.println();
-				continue;
-			} else if(codeLine.equals(printFuncsCmd)) {
-				Set<ArrayList<String>> funcs = i.evaluator.functions.keySet();
-				for(ArrayList<String> fns : funcs) {
-					for(String f : fns) {
-						System.out.print(f + " ");
-					}
-					System.out.println("=> " + i.evaluator.functions.get(fns));
-				}
-			} else if(codeLine.equals(printCmd)) {
-				System.out.println(i.input(lastLine));
-			} else i.input(codeLine);
-			lastLine = codeLine;
+			i.checkExpression(codeLine);
+			if(i.shouldBreak) break;
 		}
 		i.scanner.close();
 	}
